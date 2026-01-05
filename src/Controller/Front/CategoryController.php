bce7aa2faf23771a868ccfddfaa61a95891eff4c
Auxioma <auxioma@yandex.fr>
2026-01-05 16:43:01 +0100

    structure de la page d'accueil

---
+++ src/Controller/Front/CategoryController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller\Front;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class CategoryController extends AbstractController
+{
+    #[Route('/front/category', name: 'app_front_category')]
+    public function index(): Response
+    {
+        return $this->render('front/category/index.html.twig', [
+            'controller_name' => 'CategoryController',
+        ]);
+    }
+
+    public function navbar(): Response
+    {
+        return $this->render('front/partials/_navbar.html.twig');
+    }   
+}
